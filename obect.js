b7705599a73b89c8a19c75aeb84e65df928524e0
Amit verma <190012191+amitskverma@users.noreply.github.com>
2026-09-22 14:26:24 +0530

    multistage docker

---
+++ obect.js
@@ -0,0 +1,11 @@
+const employee ={
+    id: "E-1001" ,
+    firstName: "Amit" ,
+    lastName: "Verma",
+    deparment:"Engineernig",
+    title:" Fullstack DeV",
+    isActive: true
+
+};
+
+console.log(employee)
